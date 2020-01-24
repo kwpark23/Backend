207d970b2745c615ac83a515183cfb25c6a15981
Nada Elnour <nelnour90@gmail.com>
2020-01-23 23:23:33 -0500

    Add expiryDate, invtoryItemId, and groceryStoreId props

--- server/Item.js
+++ server/Item.js
@@ -1,6 +1,9 @@
-class Item {
+export class Item {
     constructor(itemRef) {
+        this.inventoryItemId = itemRef.inventoryItemId;
         this.name = itemRef.name;
         this.qty = itemRef.qty;
+        this.expiryDate = itemRef.expiryDate;
+        this.groceryStoreId = itemRef.groceryStoreId
     }
 }
--- server/Order.js
+++ server/Order.js
@@ -1,28 +1,32 @@
+/* Order is observer of: Inventory
+ *           observed by: Drivers
+ */
+
 class Order {
+
     constructor(orderRef) {
-        this.id = orderRef.id
-        this.progress = '';
-        this.driver = Null;
-        this.foodBankId = Null;
-        this.groceryStoreId = Null;
-        this.items = this.parseItems(orderRef.items);
-        this.requestTime = Date(orderRef.time);
+        this.orderId = orderRef.id
+        this.progressStatus = orderRef.status;
+        this.foodBankId = orderRef.foodBankId;
+        this.groceryId = orderRef.groceryId;
+        this.inventoryItems = {};
+        this.timePlaced = Date(orderRef.time);
+
+        this.driverId = {};
+
+        this.parseItems(orderRef.items);
     }
 
     setProgressStatus(newStatus) {
-        this.progress = newStatus;
+        this.progressStatus = newStatus;
     }
 
     getProgressStatus() {
-        return this.progress;
+        return this.progressStatus;
     }
 
-    setDriver(driver) {
-        this.driver = driver;
-    }
-
-    getDriver() {
-        return this.driver;
+    getDriver(driverId) {
+        return this.driverId[driverId];
     }
 
     setFoodBankId(foodBankId) {
@@ -34,29 +38,119 @@ class Order {
     }
 
     setGroceryId(groceryId) {
-        this.groceryStoreId = groceryId;
+        this.groceryId = groceryId;
     }
 
     getGroceryId() {
-        return this.groceryStoreId;
+        return this.groceryId;
     }
 
     setTime(time) {
-        this.requestTime = time;
+        this.timePlaced = time;
     }
 
     getTime() {
-        return this.requestTime;
+        return this.timePlaced;
     }
 
     getItem(itemId) {
-        return this.items;
+        return this.inventoryItems[itemId];
     }
 
-    parseItems(itemsList) {
-        itemsList.forEach(item => {
-            item = new Item(item);
-        })
+    parseItems(itemsList) { // who parses itemsJSON into Items?
+        itemsList.forEach( item => {
+            this.inventoryItems[item.name] = item
+        });
     }
 
+    // Beginning of Observable's duties
+    notifyFoodBank(foodBankURL) {
+        try {
+            let fbServer = new XMLHttpRequest();
+            fbServer.open("POST", foodBankURL, true);
+            fbServer.setRequestHeader('Content-Type', 'application/json');
+            fbServer.send(JSON.stringify({
+                "orderId": this.orderId,
+                "status": this.progressStatus
+            }))
+
+            fbServer.onreadystatechange = function () {
+                if (this.status === 200) {
+                    console.log("200");
+                }
+            }
+
+            fbServer.send();
+
+        } catch (error) {
+            console.log("Can't notify entity.")
+        }
+    }
+
+    notifyGroceryStore(groceryStoreURL) {
+        try {
+            let gsServer = new XMLHttpRequest();
+            gsServer.open("POST", groceryStoreURL, true);
+            gsServer.setRequestHeader('Content-Type', 'application/json');
+            gsServer.send(JSON.stringify({
+                "orderId": this.orderId,
+                "status": this.progressStatus
+            }))
+
+            gsServer.onreadystatechange = function () {
+                if (this.status === 200) {
+                    console.log("200");
+                }
+            }
+
+            gsServer.send();
+        } catch (error) {
+            console.log("Can't notify entity.")
+        }
+    }
+    
+
+    notifyDrivers(context) { // push notification
+        try {
+            console.log('Driver notified with ' + context);
+        } catch (error) {
+            console.log("Notification failed" + error);
+        }
+    }
+
+    addDriver(driver) { // adds observer
+        try {
+            if (this.isDriverValid(driver)) {
+                this.driverId[driver.id] = driver;
+                console.log("Driver added.");
+            }
+        } catch (error) {
+            console.log("Can't assign driver" + error);
+        }
+
+    }
+
+    removeDriver(driverId) { // remove observer
+        try {
+            delete this.driverId[driverId];
+            console.log("Driver" + driverId + " removed.");
+        } catch (error) {
+            console.log("Can't unsubscribe driver " + error);
+        }
+    }
+
+    isDriverValid(driver) {
+        //check if Driver instance has ID, name, reg. vehicle
+
+        return ((driver.id !== null) && driver.name !== undefined && driver.vehicle !== undefined);
+    }
+    // End of Observable duties
+
+    // Beginning of Observer duties
+    updateStatus(newStatus) {
+        // update properties in response to changes in Inventory
+        order.setProgressStatus(newStatus);
+    }   
+
+    // End of Observer duties
 }
