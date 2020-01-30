aab3daa108aed3729e64871c52c547422f9064a7
Nada Elnour <nelnour90@gmail.com>
2020-01-30 11:52:22 -0500

    Write and update grocery store user to firebase db

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ const functions = require('firebase-functions');
 const express = require('express');
 const Order = require('./Order');
 var groceryStores = {};
+var gsDB = firebase.database();
 
 // General request handler
 var foodBankFunctions = express();
@@ -29,3 +30,24 @@ groceryStoreFunctions.post('/sendUser', (request, response) => {
 })
 
 exports.groceryStore = functions.https.onRequest(groceryStoreFunctions)
+
+function writeGroceryStoreData(storeId, companyName, location, storeNumber) {
+    gsDB.ref('groceryStores/' + storeId).set({
+    companyName: companyName,
+    location: location,
+    storeNumber: storeNumber
+    });
+}
+
+function updateGroceryStoreData(storeId, companyName, location, storeNumber) {
+    var update = {};
+    var updatedInfo = {
+        companyName: companyName,
+        location: location,
+        storeNumber: storeNumber
+    }
+
+    update['groceryStores/' + storeId] = updatedInfo;
+
+    gsDB.ref().update(update);
+}
