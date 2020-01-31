5a0192f1b5e90fcfe6d63c1c9bb07854c1897e56
Nada Elnour <nelnour90@gmail.com>
2020-01-31 10:32:21 -0500

    Change MainServer to OrderProcessor and add processOrder method

--- server/OrderProcessor.js
+++ server/OrderProcessor.js
@@ -4,7 +4,7 @@ class MainServer {
         this.activeDriver = {};
     }
 
-    addOrderToDict(order, gs) {
+    processOrder(order, gs) {
         gs.updateStatus(order);
 
         if (order.status === 'Invalid') {
@@ -14,6 +14,10 @@ class MainServer {
             this.activeOrder[order.orderId] = order;
             console.log("Order created")
         }
+    }
+
+    addOrderToDict(order) {
+        this.activeOrder[order.orderId] = order;
     }
 
     addDriverToDict(driver) {
