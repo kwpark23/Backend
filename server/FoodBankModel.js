dc9f174120b51520033137d66100e1620e5946f9
izbaam <59741251+izbaam@users.noreply.github.com>
2020-02-06 13:34:32 -0500

    SEN-39 Food Bank Model Class

---
+++ server/FoodBankModel.js
@@ -0,0 +1,8 @@
+class FoodBankModel{
+    contructor(FoodBankID, fbName, location, locationNumber){
+        this.FoodBankID= FoodBankID,
+        this.fbName = fbName,
+        this.location=location,
+        this.locationNumber=locationNumber;
+    }
+}
